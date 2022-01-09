0264240870ca60b672670e09048e02f3c37ff28c
SimonRenggli1 <96227533+SimonRenggli1@users.noreply.github.com>
2022-01-09 22:36:04 +0100

    Add files via upload

--- NotenRechner.py
+++ NotenRechner.py
@@ -1,6 +1,6 @@
 anzahlTests = float(input("Wie viele Tests hattest du schon? "))
 noteJetzt = float(input("Welche Note hast du jetzt? "))
-noteGewollt = float(input("Welche Note möchtest du?"))
+noteGewollt = float(input("Welche Note möchtest du? "))
 noteSumme = noteJetzt * anzahlTests
 newAnzahlTests = anzahlTests + 1
 noteGesucht = noteGewollt * newAnzahlTests - noteSumme
@@ -11,3 +11,4 @@ if noteGesucht <= 6:
 elif noteGesucht > 6:
     print("Leider schaffst du es nicht mehr :/")
     print("Du bräuchtest eine " + str(noteGesucht) + " für eine " + str(noteGewollt))
+input("Press enter to coninue...")
